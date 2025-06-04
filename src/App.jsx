5cd2f21acefcbf28562fcd29374cf3f8b0aefcdf
people9953 <people99532@gmail.com>
2025-06-04 09:29:53 +0900

    코드 수정및 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import "./App.css";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <main>
+    <main className="max-w-[1280px] mx-auto p-8 text-center">
       <Header />
-      <section>
+      <section className="flex flex-wrap justify-center gap-5 px-5 py-10">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
